cd44c35bf671ea475a0fbdcd857dd63bcfe4720f
husainwafaie <117494930+husainwafaie@users.noreply.github.com>
2024-10-31 08:35:57 -0700

    Finished subject class

--- tutor-booking/src/main/java/com/example/tutor_booking/model/Subject.java
+++ tutor-booking/src/main/java/com/example/tutor_booking/model/Subject.java
@@ -1,5 +1,41 @@
 package com.example.tutor_booking.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Subject {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "subjects")
+    private Set<Tutor> tutors = new HashSet<>();
+
+    public Subject(String name) {
+        this.name = name;
+    }   
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Tutor> getTutors() {
+        return tutors;
+    }
+
+    public void setTutors(Set<Tutor> tutors) {
+        this.tutors = tutors;
+    }
 }
