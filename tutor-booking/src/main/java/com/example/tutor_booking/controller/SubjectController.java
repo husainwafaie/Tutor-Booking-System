1d376e4ba8c7ad24ab8ab770626a7fc29fa55f93
husainwafaie <117494930+husainwafaie@users.noreply.github.com>
2024-11-02 08:43:49 -0700

    Created Controller for Subject  model

---
+++ tutor-booking/src/main/java/com/example/tutor_booking/controller/SubjectController.java
@@ -0,0 +1,32 @@
+package com.example.tutor_booking.controller;
+
+import com.example.tutor_booking.model.Subject;
+import com.example.tutor_booking.repository.SubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subjects")
+public class SubjectController {
+
+    private final SubjectRepository subjectRepository;
+
+    @Autowired
+    public SubjectController(SubjectRepository subjectRepository) {
+        this.subjectRepository = subjectRepository;
+    }
+
+    // GET all subjects
+    @GetMapping
+    public List<Subject> getAllSubjects() {
+        return subjectRepository.findAll();
+    }
+
+    // POST a new subject
+    @PostMapping
+    public Subject createSubject(@RequestBody Subject subject) {
+        return subjectRepository.save(subject);
+    }
+}
