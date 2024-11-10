af9804c2eb6dc96639248f966147e9c71f72ddbb
husainwafaie <117494930+husainwafaie@users.noreply.github.com>
2024-11-10 14:51:38 -0800

    Add app and navbar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <Link to="/">Tutor Booking System</Link>
+            </div>
+            <ul className="navbar-links">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/add-student">Add Student</Link></li>
-                {/* more to add*/}
+                <li><Link to="/signup">Sign Up</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/search-subject">Find Subject</Link></li>
+                <li><Link to="/search-tutor">Find Tutor</Link></li>
+                <li><Link to="/book-appointment">Book Appointment</Link></li>
             </ul>
         </nav>
     );
